5c3ef2b3202800d8675807b932ac0cbc8e10f9fa
Pranjal Ambwani <PRANJAL.AMBWANI012@nmims.in>
2026-04-09 14:05:43 +0530

    Delete core directory

--- core/__init__.py
+++
@@ -1,9 +0,0 @@
-from .inference import get_predictor, RentPredictor
-from .database import get_db, RentIQDatabase
-from .features import build_features, listing_to_vector, FEATURE_NAMES
-
-__all__ = [
-    "get_predictor", "RentPredictor",
-    "get_db", "RentIQDatabase",
-    "build_features", "listing_to_vector", "FEATURE_NAMES",
-]
